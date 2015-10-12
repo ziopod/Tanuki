a47cd17726e855b9dde629c4dbd1b89773e5db9e
Ziopod <ziopod@gmail.com>
2015-10-12 23:43:45 +0200

    accept dash in file name

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -152,7 +152,7 @@ Kohana::modules(array(
 Route::set('pages', '<slug>', array(
 		// 'slug'	=> 'my_page', // restrict to a specific url
 		// 'slug'	=> '.*', // for any extension in url
-		'slug'	=> '[a-zA-Z0-9_/]+', // for subfolder
+		'slug'	=> '[a-zA-Z0-9_\-/]+', // for subfolder
 	))
 	->defaults(array(
 		'controller'	=> 'Pages',
